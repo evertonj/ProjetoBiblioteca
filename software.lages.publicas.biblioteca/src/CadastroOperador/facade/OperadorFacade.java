bcf47192d6dab6efe0a7fc4979e05dc9efe26cfe
evertonj <evertonspindola@gmail.com>
2014-07-09 01:26:35 -0300

    Novas Atualização das telas
    Novas Telas

---
+++ software.lages.publicas.biblioteca/src/CadastroOperador/facade/OperadorFacade.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CadastroOperador.facade;
+
+import CadastroOperador.dao.IOperadorDAO;
+import CadastroOperador.dao.OperadorDAO;
+import CadastroOperador.entity.Operador;
+import java.util.List;
+
+/**
+ *
+ * @author Alex
+ */
+public class OperadorFacade {
+
+    private IOperadorDAO dao;
+
+    public OperadorFacade() {
+        this.dao = new OperadorDAO();
+    }
+
+    public int save(Operador operador) {
+        return dao.save(operador);
+    }
+
+    public int update(Operador operador) {
+        return dao.update(operador);
+    }
+
+    public int remove(Long id) {
+        return dao.remove(id);
+    }
+    
+    public Operador search(String nome) {
+        return dao.search(nome);
+    }
+
+    public List<Operador> finAll() {
+        return dao.findAll();
+    }
+}
