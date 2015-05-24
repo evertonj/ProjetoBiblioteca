723c47b0dd260c7c949190f5b20daad9c2889e2b
Everton Spindola <evertonspindola@gmail.com>
2015-05-24 16:54:28 -0300

    Criada a entidade Reserva.

---
+++ src/entity/Reserva.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Everton
+ */
+public class Reserva {
+    private Usuario usuario;
+    private Obra obra;
+    private Calendar dataReserva;
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the obra
+     */
+    public Obra getObra() {
+        return obra;
+    }
+
+    /**
+     * @param obra the obra to set
+     */
+    public void setObra(Obra obra) {
+        this.obra = obra;
+    }
+
+    /**
+     * @return the dataReserva
+     */
+    public Calendar getDataReserva() {
+        return dataReserva;
+    }
+
+    /**
+     * @param dataReserva the dataReserva to set
+     */
+    public void setDataReserva(Calendar dataReserva) {
+        this.dataReserva = dataReserva;
+    }
+}
